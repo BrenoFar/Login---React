5b2ef8ecb37a561b8ba1c94f4a5e2c8d37eb2b69
Breno Farias <brenodasfarias2001@gmail.com>
2023-06-18 23:47:43 -0300

    updates

--- Server-side/models/User.js
+++ Server-side/models/User.js
@@ -1,39 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true
-        }
-      },
-      firstName: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      lastName: {
-        type: DataTypes.STRING
-      },
-      role: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      status: {
-        type: DataTypes.STRING,
-        allowNull: false
-      }
-    });
-  
-    return User;
-  };
-  
+  const User = sequelize.define('User', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    password: DataTypes.STRING
+  }, {});
+  return User;
+}
--- Server-side/routes/User.js
+++ Server-side/routes/User.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { User } = require('../models');
 
-
+// GET /user 
 
 router.post('/', async (req, res) => {
   try {
