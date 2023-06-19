7329fa2af847051991e7b2893aa1246b1e71939f
Breno Farias <brenodasfarias2001@gmail.com>
2023-06-19 00:40:18 -0300

    operation successful (db inserting objects)

--- Server-side/index.js
+++ Server-side/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
 const db = require('./models');
+
+app.use(bodyParser.json()); // Para analisar corpos de requisições com formato JSON
+app.use(bodyParser.urlencoded({ extended: true })); // Para analisar corpos de requisições com formato x-www-form-urlencoded
+
 
 // Routers
 const userRouter = require('./routes/User');
--- Server-side/routes/User.js
+++ Server-side/routes/User.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { User } = require('../models');
 
+
+
 //Routers
 
 // GET /user 
