eb105e7d8ced06dc9393e644c6074172631e1aaf
Breno Farias <brenodasfarias2001@gmail.com>
2023-06-19 00:44:58 -0300

    making changes to data in DB only allow if value is not null

--- Server-side/models/User.js
+++ Server-side/models/User.js
@@ -1,13 +1,24 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
+    username: {
+      type:DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    firstName: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    lastName:{
+      type: DataTypes.STRING,
+      allowNull: false},
     email: {
       type: DataTypes.STRING,
       unique: true
     },
-    password: DataTypes.STRING
+    password: {
+      type:DataTypes.STRING,
+      allowNull: false}
   }, {});
   return User;
 }
